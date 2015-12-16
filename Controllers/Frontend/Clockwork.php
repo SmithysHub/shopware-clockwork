25381b7494df83e7eb7c78073b4c452d19dcc100
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2015-12-16 22:57:40 +0100

    Controller
    Add pre- and post-Dispatch logic for Return JSON

--- Controllers/Frontend/Clockwork.php
+++ Controllers/Frontend/Clockwork.php
@@ -2,9 +2,30 @@
 
 class Shopware_Controllers_Frontend_Clockwork extends Enlight_Controller_Action
 {
+
+    public function preDispatch()
+    {
+        $this->Front()->Plugins()->ViewRenderer()->setNoRender();
+    }
+
+    public function postDispatch()
+    {
+        $data = $this->View()->getAssign();
+        $pretty = $this->Request()->getParam('pretty', false);
+
+        $data = Zend_Json::encode($data);
+        if ($pretty) {
+            $data = Zend_Json::prettyPrint($data);
+        }
+
+        $this->Response()->setHeader('Content-type', 'application/json', true);
+        $this->Response()->setBody($data);
+    }
+
     public function indexAction()
     {
         $id = $this->Request()->getParam('id', null);
     }
+
 
 }
