1de15655b4d7c50d3fc928f4f258d047c655518f
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2015-12-29 23:10:55 +0100

    Put Logic in to shopware data source

--- Clockwork/DataSource/ShopwareDataSource.php
+++ Clockwork/DataSource/ShopwareDataSource.php
@@ -3,27 +3,143 @@ namespace Shopware\Plugins\ShopwareClockwork\Clockwork\DataSource;
 
 use Clockwork\DataSource\DataSource;
 use Clockwork\Request\Request;
+use Clockwork\Clockwork;
+use Psr\Log\LogLevel;
 
 class ShopwareDataSource extends DataSource
 {
-    protected $context;
+    /**
+     * @var \Shopware\Components\DependencyInjection\Container
+     */
+    protected $container;
 
     /**
-     * ShopwareDataSource constructor.
-     * @param $context
+     * @var array
      */
-    public function __construct($context)
+    protected $data = [
+        'viewsData' => [],
+        'log' => [],
+        'databaseQueries' => [],
+        ''
+    ];
+
+    /**
+     * ClockworkLogger constructor.
+     */
+    public function __construct()
     {
-        $this->context = $context;
+        $this->container = Shopware()->Container();
+    }
+
+    public function setBaseInfo( \Enlight_Controller_Request_RequestHttp $request ) {
+        $this->data['controller'] = $request->getControllerName();
+        $this->data['getData'] = $request->getQuery();
+        $this->data['postData'] = $request->getPost();
+    }
+
+    /**
+     * @param $label
+     * @param array $data
+     */
+    public function formatViewData($label, array $data)
+    {
+        $this->data['viewsData'][] = ['data' => [
+            'name' => $label,
+            'data' => ''
+        ]];
+
+        array_shift($data);
+        foreach ($data as $item) {
+            $this->data['viewsData'][] = ['data' => [
+                'name' => $item[0],
+                'data' => $item[1]
+            ]];
+        }
+    }
+
+    /**
+     * @param array $data
+     */
+    public function formatErrorData(array $data)
+    {
+        array_shift($data);
+        foreach ($data as $item) {
+            $level = LogLevel::INFO;
+            $message = $item[2] . ' | ' . $item[3] . ' in: ' . $item[5] . ':' . $item[4];
+            if ( $item[2] === 'E_WARNING' ) {
+                $level = LogLevel::WARNING;
+            } elseif( $item[2] === 'E_RECOVERABLE_ERROR' ){
+                $level = LogLevel::ERROR;
+            } elseif( $item[2] === 'E_NOTICE' ){
+                $level = LogLevel::NOTICE;
+            }
+            $this->getClockWork()->log($level, $message);
+        }
+    }
+
+    public function formatSqlQuerys($label, array $data)
+    {
+        array_shift($data);
+        $this->data['databaseQueries'][] = [
+            'query' => $label,
+            'duration' => 0
+        ];
+
+        foreach ($data as $item) {
+            $query = $item[2];
+            if( !empty($item[3]) ) {
+                $query .= ' | Params: ' . json_encode($item[3]);
+            }
+            if( $item[1] > 1 ) {
+                $query .= ' | Count: ' . $item[1];
+            }
+            $this->data['databaseQueries'][] = [
+                'query' => $query,
+                'duration' => (float)$item[0]
+            ];
+        }
+    }
+
+    public function setControllerEventTimeline(array $data){
+        $clockWork = $this->getClockWork();
+
+        array_shift($data);
+        foreach ($data as $item) {
+            $clockWork->getTimeline()->addEvent($item[0], $item[0], $item[3], $item[2] + $item[3]);
+        }
+
     }
 
 
+    public function setControllerTimeline(array $data){
+        $clockWork = $this->getClockWork();
+
+        array_shift($data);
+        foreach ($data as $item) {
+            $clockWork->getTimeline()->addEvent($item[0], $item[0], $item[4], $item[4] + $item[2] + $item[3]);
+        }
+
+    }
+
+    public function setEventInfo(array $data){
+        $clockWork = $this->getClockWork();
+
+        array_shift($data);
+        foreach ($data as $item) {
+            $clockWork->getTimeline()->addEvent($item[0], $item[0], $item[4], $item[4] + $item[2]);
+            $clockWork->log($item[0], $item[3]);
+        }
+    }
+
     /**
      * the entry-point. called by Clockwork itself.
+     *
+     * @param Request $request
+     * @return Request
      */
     public function resolve(Request $request)
     {
-        $data = $this->context->getData();
+        $data = $this->getData();
         foreach ($data as $name => $item) {
             if($name === 'postData') {
                 $item = $this->removePasswords($item);
@@ -33,4 +149,20 @@ class ShopwareDataSource extends DataSource
 
         return $request;
     }
+
+    /**
+     * @return array
+     */
+    protected function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @return Clockwork
+     */
+    protected function getClockWork(){
+        return $this->container->get('shopwareclockwork.clockwork');
+    }
+
 }
