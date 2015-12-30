dec5f42fa27326f3558d72e8ffdd8254267e68ef
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2015-12-30 08:45:36 +0100

    Add subscriber for debug plugin

---
+++ Subscriber/Debug.php
@@ -0,0 +1,67 @@
+<?php
+namespace Shopware\Plugins\ShopwareClockwork\Subscriber;
+
+use Enlight\Event\SubscriberInterface;
+use Shopware\Plugins\ShopwareClockwork\Clockwork\Components\Log\ClockworkHandler;
+use Shopware\Plugins\ShopwareClockwork\Clockwork\Components\Collector\EventCollector as ClockworkEventCollector;
+use Shopware\Plugins\ShopwareClockwork\Clockwork\Components\Collector\TemplateCollector as ClockworkTemplateCollector;
+use Shopware\Plugin\Debug\Components\EventCollector;
+use Shopware\Plugin\Debug\Components\TemplateCollector;
+
+class Debug implements SubscriberInterface
+{
+
+    /**
+     * @var \Shopware\Components\DependencyInjection\Container
+     */
+    protected $container;
+
+    /**
+     * Container constructor.
+     */
+    public function __construct()
+    {
+        $this->container = Shopware()->Container();
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Shopware_Plugins_Core_Debug_Bootstrap_FilterHandlerRegister' => 'onFilterHandlerRegister',
+            'Shopware_Plugins_Core_Debug_Bootstrap_FilterCollectors' => 'onFilterCollectors'
+        ];
+    }
+
+    public function onFilterHandlerRegister(\Enlight_Event_EventArgs $arguments)
+    {
+        $handlerRegister = $arguments->getReturn();
+
+        $handlerRegister[] = new ClockworkHandler();
+
+        return $handlerRegister;
+    }
+
+    public function onFilterCollectors(\Enlight_Event_EventArgs $arguments)
+    {
+        $collectors = $arguments->getReturn();
+        $utils = $arguments->get('utils');
+
+        foreach ($collectors as $key => $collector) {
+            if( $collector instanceof EventCollector) {
+                $collectors[$key] = new ClockworkEventCollector(
+                    $arguments->get('eventManager'),
+                    $utils
+                );
+            } elseif( $collector instanceof TemplateCollector) {
+                $collectors[$key] = new ClockworkTemplateCollector(
+                    $this->container->get('template'),
+                    $utils,
+                    $this->container->get('kernel')->getRootDir()
+                );
+            }
+        }
+
+        return $collectors;
+    }
+
+}
